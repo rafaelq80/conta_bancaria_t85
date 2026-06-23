7b6ad1deeda8ca4fa4f9152d3bb01fae4886184c
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-06-23 09:45:34 -0300

    :sparkles: Construção da Model ContaCorrente

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -3,6 +3,7 @@ package conta_bancaria;
 import java.util.Scanner;
 
 import conta_bancaria.model.Conta;
+import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.util.Cores;
 
 public class Menu {
@@ -17,7 +18,7 @@ public class Menu {
 		Conta c1 = new Conta(1, 123, 1, "Isabella", 200000.00f);
 		c1.visualizar();
 		
-		Conta c2 = new Conta(1, 123, 2, "Thiago", 100000.00f);
+		Conta c2 = new Conta(2, 123, 2, "Thiago", 100000.00f);
 		c2.visualizar();
 		
 		/* Alteração do Saldo*/
@@ -46,7 +47,20 @@ public class Menu {
 		c2.depositar(50000.00f);
 		c2.visualizar();
 	
+		/* Instanciar Objetos da Classe ContaCorrente*/
+		
+		ContaCorrente cc1 = new ContaCorrente(3, 789, 1, "Raquel", 200000.00f, 2000.00f);
+		cc1.visualizar();
+		
+		System.out.println("\nSacar R$ 203.000,00 da conta Cc1: " + (cc1.sacar(203000.00f) ? 
+				"Saque efetuado com sucesso! | Saldo: " + cc1.getSaldo() : "Saldo Insuficiente | Saldo: " + cc1.getSaldo()));
+		
+		System.out.println("\nSacar R$ 202.000,00 da conta Cc1: " + (cc1.sacar(202000.00f) ? 
+				"Saque efetuado com sucesso! | Saldo: " + cc1.getSaldo() : "Saldo Insuficiente | Saldo: " + cc1.getSaldo()));
 
+		cc1.depositar(2000.00f);
+		cc1.visualizar();
+		
 		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
